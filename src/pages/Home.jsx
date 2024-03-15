e30cf665c7d0850ea32df38ae3423e9411dfe13d
Michal Karczewicz <michu.karcz@wp.pl>
2024-03-15 10:16:01 +0100

    Added popup - more details about exercise

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import homePage from "../assets/homePageImage.jpg";
-import { RiSearchLine } from "react-icons/ri";
+import { RiSearchLine, RiCloseCircleLine } from "react-icons/ri";
 
 const Home = () => {
   const [exercises, setExercises] = useState([]);
   const [selectedExercises, setSelectedExercises] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [selectedExercise, setSelectedExercise] = useState(null);
+  const [popupHeight, setPopupHeight] = useState("auto");
 
   const fetchData = async () => {
     try {
@@ -33,6 +35,22 @@ const Home = () => {
     setSearchResults(filteredExercises);
   };
 
+  const handleOpenPopup = (exercise) => {
+    setSelectedExercise(exercise);
+    const instructionsElement = document.getElementById("instructions");
+    if (
+      instructionsElement &&
+      instructionsElement.offsetHeight > window.innerHeight * 0.7
+    ) {
+      setPopupHeight("70vh");
+    }
+  };
+
+  const handleClosePopup = () => {
+    setSelectedExercise(null);
+    setPopupHeight("auto");
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -43,7 +61,7 @@ const Home = () => {
 
   return (
     <div className="w-full h-full bg-gray-300">
-      <div className="relative w-full h-[60vh]">
+      <div className="relative w-full h-[60vh] p-0 m-0">
         <img
           src={homePage}
           alt="HomePageImage"
@@ -69,9 +87,10 @@ const Home = () => {
           {(searchTerm ? searchResults : selectedExercises).map((exercise) => (
             <div
               key={exercise.id}
-              className="bg-white grid grid-cols-2 p-4 rounded-lg transform hover:scale-105 transition-transform"
+              className="bg-white grid grid-cols-2 p-4 rounded-lg transform hover:scale-105 transition-transform cursor-pointer"
+              onClick={() => handleOpenPopup(exercise)}
             >
-              <div>
+              <div className="col-span-1">
                 <img
                   src={`/exercisesDir/${exercise.images[0]}`}
                   alt={exercise.name}
@@ -79,7 +98,7 @@ const Home = () => {
                 />
               </div>
 
-              <div className="text-center text-gray-800 flex justify-center items-center flex-col mx-8">
+              <div className="col-span-1 text-center text-gray-800 flex justify-center items-center flex-col mx-8">
                 <h2 className="text-xl m-3 text-red-700 font-bold whitespace-no-wrap overflow-hidden">
                   {exercise.name}
                 </h2>
@@ -90,6 +109,63 @@ const Home = () => {
           ))}
         </div>
       </div>
+      {selectedExercise && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50"
+          onClick={handleClosePopup}
+        >
+          <div
+            className="bg-white p-6 rounded-lg relative max-w-3xl overflow-y-auto"
+            style={{ maxHeight: popupHeight }}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <RiCloseCircleLine
+              className="absolute top-3 right-3 cursor-pointer text-gray-400 hover:text-red-500"
+              onClick={handleClosePopup}
+            />
+            <h2 className="text-2xl font-semibold mb-4 text-red-600">
+              {selectedExercise.name}
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+              <div className="col-span-1">
+                <img
+                  src={`/exercisesDir/${selectedExercise.images[0]}`}
+                  alt={selectedExercise.name}
+                  className="w-full h-48 object-cover rounded-md mb-4"
+                />
+              </div>
+              <div className="col-span-1">
+                <p className="text-lg font-semibold mb-2 text-blue-600">
+                  Level: {selectedExercise.level}
+                </p>
+                {selectedExercise.equipment && (
+                  <p className="text-lg font-semibold mb-2 text-purple-600">
+                    Equipment: {selectedExercise.equipment}
+                  </p>
+                )}
+                {selectedExercise.primaryMuscles.length > 0 && (
+                  <p className="text-lg font-semibold mb-2 text-blue-600">
+                    Primary muscles:{" "}
+                    {selectedExercise.primaryMuscles.join(", ")}
+                  </p>
+                )}
+                {selectedExercise.secondaryMuscles.length > 0 && (
+                  <p className="text-lg font-semibold mb-2 text-green-600">
+                    Secondary muscles:{" "}
+                    {selectedExercise.secondaryMuscles.join(", ")}
+                  </p>
+                )}
+              </div>
+              <div className="col-span-2">
+                <p className="text-lg font-semibold mb-2 text-red-600">
+                  Instructions:
+                </p>
+                <p id="instructions">{selectedExercise.instructions}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
